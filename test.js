daa9f7044d7e969e4b3413066c996bd59a19988a
rknoplock <127435435+rknoplock@users.noreply.github.com>
2025-04-03 11:35:35 -0300

    Create test.js

---
+++ test.js
@@ -0,0 +1,10 @@
+
+// tests/test.js
+const request = require('supertest');
+const app = require('../app');
+
+test('GET / retorna Hello, CI/CD!', async () => {
+  const response = await request(app).get('/');
+  expect(response.statusCode).toBe(200);
+  expect(response.text).toBe('Hello, CI/CD!');
+});
