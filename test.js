6dc7678e3f7344b10d5aa49313cfb59afddb1114
rknoplock <127435435+rknoplock@users.noreply.github.com>
2025-04-03 13:12:35 -0300

    Delete test.js

--- test.js
+++
@@ -1,8 +0,0 @@
-const request = require('supertest');
-const app = require('./app'); // Importa o app corretamente
-
-test('GET / retorna Hello, CI/CD!', async () => {
-  const response = await request(app).get('/');
-  expect(response.statusCode).toBe(200);
-  expect(response.text).toBe('Hello, CI/CD!');
-});
